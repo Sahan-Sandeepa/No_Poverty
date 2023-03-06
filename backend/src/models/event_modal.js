c3a6f5fab4891daa2aa4a47b7becea82216b5f88
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-03-07 00:41:04 +0530

    created event modal

---
+++ backend/src/models/event_modal.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({eventNo: {type: String,required: true,unique: true,//backend validation
+    },
+
+    eventName: {type: String,require: true,},
+
+    eventPlace: {type: String,require: true,},
+
+    eventDetails: {type: String,require: true,},
+
+    eventDate: {type: String,require: true,},
+
+    eventStatus: {type: String,require: true,},
+});
+const event = mongoose.model("event", eventSchema);
+module.exports = event; //export...................
