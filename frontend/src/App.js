8fa041227740b8c78cca8a837fe4f4da1af0898b
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-03-15 22:26:58 +0530

    changed app_js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Header from './components/header';
 import Header_bar from './components/header_bar';
 import AddFinancial from './components/financial/AddFinancial';
 import Financial from './components/financial/financial';
+import AllEvent from './components/Event/Admin/AllEvent';
+import AddEvent from './components/Event/Admin/AddEvent';
+import UpdateEvent from './components/Event/Admin/UpdateEvent';
 
 function App() {
   return (
@@ -17,6 +20,14 @@ function App() {
       <Route path='/addfinancial'
       element={<AddFinancial/>}
       />
+
+      {/* ==========================Sahan Routes========================== */}
+
+        <Route path='/eventMain' element={<AllEvent />} />
+        <Route path='/addEvent' element={<AddEvent />} />
+        <Route path='/updateEvent' element={<UpdateEvent />} />
+
+      {/* ==========================Sahan Routes========================== */}
       </Routes>
 
       </BrowserRouter>
