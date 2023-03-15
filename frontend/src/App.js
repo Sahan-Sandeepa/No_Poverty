02e4e3ebf68921e111eec40a52f2458903016052
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-03-15 12:49:05 +0530

    changed app_js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Header_bar from './components/header_bar';
 import AddFinancial from './components/financial/AddFinancial';
+import AddEvent from './components/Event/Admin/AddEvent';
+import AllEvent from './components/Event/Admin/AllEvent';
 
 function App() {
   return (
     <div>
-      <Header />
+      <AddEvent />
+      <AllEvent />
     </div>
   );
 }
