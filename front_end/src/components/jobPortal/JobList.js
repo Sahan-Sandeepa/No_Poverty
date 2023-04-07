93072dd2b5250bce95e8c6d5e22d4181a1deb97d
IsuruSanjaya <isurusanjayaa@gmail.com>
2023-04-07 22:15:58 +0530

    fix job list

--- front_end/src/components/jobPortal/JobList.js
+++ front_end/src/components/jobPortal/JobList.js
@@ -5,7 +5,7 @@ import { EditTwoTone, DeleteOutlined, DeleteTwoTone, DownloadOutlined, FilePdfOu
 import CustomRow from '../common/Form_header';
 import WrapperCard from '../common/Wrapper_card';
 import JobPost from './JobPost';
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 
 
 const { Search } = Input;
@@ -21,11 +21,16 @@ const JobList = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
+    const [searchText, setSearchText] = useState("");
+
+    const history = useNavigate();
 
 
+    //popup modal method.if add order click modal pop out
     const addOrder = async () => {
         setIsModalOpen(false);
         setOpenEditOrderModal(false);
+        refresh();
     }
     const showModal = () => {
         setIsModalOpen(true);
@@ -34,11 +39,15 @@ const JobList = () => {
         setIsModalOpen(false);
         setIsEditModalOpen(false);
     };
+
+    //modal cancel button 
     const handleCancel = () => {
         setIsModalOpen(false);
         setIsEditModalOpen(false);
 
     };
+
+    //retireve all the  data
     function getJobList() {
         axios.get("http://localhost:4000/jobHire/")
             .then((res) => {
@@ -53,22 +62,24 @@ const JobList = () => {
     }, [])
 
 
-    const onSearch = (value) => console.log(value);
+    //delete method 
 
-    // const columns = [{
-    //     title: 'Donation Name',
-    //     dataIndex: 'name',
-    //     key: 'name',
-    //     render: text => <a href="#">{text}</a>,
-    // }, {
-    //     title: 'Fund',
-    //     dataIndex: 'address',
-    //     key: 'address',
-    // },
-    // ];
-    
+    const handleDelete = async (_id) => {
+        setIsDeleteModalOpen(true)
+        axios.delete("http://localhost:4000/jobHire/delete/" + _id)
+            .then((result) => {
+                console.log("Deleted", result);
+                // window.location.reload();
+                // history.push(history.location.Financial);
 
-    const Columns=[{
+            }).catch((err) => {
+                console.log(err);
+            })
+    };
+
+    //tables header
+
+    const Columns = [{
         title: 'Job Title',
         dataIndex: 'jobTitle',
         key: 'jobTitle',
@@ -96,12 +107,18 @@ const JobList = () => {
         render: (text, record) => (
             <span>
 
-                <Button icon={<EditTwoTone />}></Button>
-                <Button icon={<DeleteOutlined style={{ fontSize: '16px', color: 'red' }} />}></Button>
+                <Button icon={<EditTwoTone key={record._id} />} onClick={() => {
+                    setIsEditModalOpen(true);
+                    setSelectedItem(record)
+                }} >
+                </Button>
 
-                {/* <a href="#">Action 一 {record.name}</a>
-                <span className="ant-divider" />
-                <a href="#">Delete</a> */}
+                <Button icon={<DeleteOutlined style={{ color: 'red' }} />}
+                    onClick={() => {
+                        handleDelete(record._id);
+                        setRefresh(true);
+                    }}
+                />
             </span>
         ),
     }];
@@ -113,53 +130,51 @@ const JobList = () => {
             <br></br>
 
             <div style={{ paddingLeft: 150 }} >
-            <div style={{ paddingLeft: 870 }} >
+                <div style={{ paddingLeft: 870 }} >
                     <Button onClick={() => { setIsModalOpen(true) }} type="primary">Create Report</Button>
 
 
                 </div>
                 <br></br>
                 <br></br>
-                <div style={{ paddingLeft: 50 }} >
-                    <div style={{ padding: 1, alignItems: "center", width: 900, height: 650, borderRadius: 5 }}>
-                        {/* <Col span={50} />
-            <Col span={30}> */}
-                        <WrapperCard style={{ backgroundColor: "#37475E" }}>
-                            <CustomRow style={{ justifyContent: "space-between", padding: "16px" }} >
-                                <h1 style={{ color: "White" }}>Job Vacancies</h1>
-                                <Col span={12} />
-                                <Search
-                                    placeholder="input search text"
-                                    onSearch={onSearch}
-                                    style={{
-                                        width: 250,
-                                    }}
-                                />
-                                <Button icon={<FilePdfOutlined style={{ fontSize: '21px', color: 'red' }} />} />
-                            </CustomRow>
-                        </WrapperCard>
-                        <Table columns={Columns} dataSource={jobList}
-                        // bordered
-                        // title={() => 'Financial Details'}
-                        />
-                        {/* </Col> */}
+                <div style={{ padding: 1, alignItems: "center", width: 1000, height: 650, borderRadius: 5 }}>
+                    <WrapperCard style={{ backgroundColor: "#37475E" }}>
+                        <CustomRow style={{ justifyContent: "space-between", padding: "10px" }} >
+                            <h1 style={{ color: "White",fontSize: 18 }}>Job Vacancies</h1>
+                            <Col span={12} />
+                            <Search
+                                placeholder="Input search text"
+                                onChange={(e) => setSearchText(e.target.value)}
+                                style={{
+                                    width: 250,
+                                }}
 
-                        <JobPost
-                            isOpen={isModalOpen}
-                            handleCancel={handleCancel}
-                            handleOk={addOrder}
+                            />
+                            <Button icon={<FilePdfOutlined style={{ fontSize: '21px', color: 'red' }} />} />
+                        </CustomRow>
+                    </WrapperCard>
+                    <Table columns={Columns} dataSource={jobList.filter((jobList) =>
+                        jobList.jobTitle.toLowerCase().includes(searchText.toLowerCase())
+                    )} />
 
-                        />
-                        <JobPost
-                            isOpen={isEditModalOpen}
-                            handleCancel={handleCancel}
-                            handleOk={addOrder}
-                            selectedItem={selectedItem}
-                        />
-                    </div>
+                    {/* passig data to Job post using props */}
+
+                    <JobPost
+                        isOpen={isModalOpen}
+                        handleCancel={handleCancel}
+                        handleOk={addOrder}
+
+                    />
+
+                    <JobPost
+                        isOpen={isEditModalOpen}
+                        handleCancel={handleCancel}
+                        handleOk={addOrder}
+                        selectedItem={selectedItem}
+                    />
                 </div>
-
             </div>
+
         </>
     )
 }
