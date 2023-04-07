49b487aca46782fbf62a83521b420108cf3dd901
GRieZzLEO <chamodhleo1@gmail.com>
2023-04-07 23:48:15 +0530

    implemented report generation in advertistments

--- front_end/src/components/DonationAdmin/ShowAllAds.js
+++ front_end/src/components/DonationAdmin/ShowAllAds.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { Table, Icon, Button, Space, Input, Col } from 'antd';
+import { Table, Icon, Button, Space, Input, Col,Row } from 'antd';
 import axios from "axios";
 import { EditTwoTone, DeleteOutlined, DeleteTwoTone, DownloadOutlined, FilePdfOutlined, FilePdfTwoTone, SelectOutlined, MessageOutlined } from '@ant-design/icons';
 import CustomRow from '../common/Form_header';
 import WrapperCard from '../common/Wrapper_card';
 import { Link, useParams } from 'react-router-dom'
 import PublishAd from './PublishAd';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable'
 
 const { Search } = Input;
 
@@ -13,6 +15,8 @@ const { Search } = Input;
 const Ads = () => {
     const [ads, setAds] = useState([]);
     const [column, setColumns] = useState([]);
+
+    const [adpdf, setAdPdf] = useState([]);
 
     const [deleteModalOpen, setIsDeleteModalOpen] = useState(false)
     const [openEditOrderModal, setOpenEditOrderModal] = useState(false);
@@ -62,6 +66,56 @@ const Ads = () => {
                 console.log(err);
             })
     };
+
+    const generatePdf = () => {
+        const watermarkTitle = 'Advertistment Summary Report';
+        // Create the PDF document
+        var doc = new jsPDF();
+        doc.setFontSize(10);
+        doc.setFont('helvetica', 'bold');
+        doc.text(10, 10, 'Donation Advertistments Summary');
+        doc.setFillColor(220, 220, 220);
+        doc.rect(0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight(), 'F');
+
+        //header and columns of the pdf
+        doc.autoTable(
+            {
+                columns: [
+                    { header: 'Name', dataKey: 'name' },
+                    { header: ' Location', dataKey: 'location' },
+                    { header: 'Small Description', dataKey: 'smallDes' },
+                    { header: 'Long Description', dataKey: 'longDes' },
+                    { header: 'Help Type Required', dataKey: 'help' },
+                    
+
+
+                ],
+                body: ads.map(ads => {
+                    return {
+                        Row: Row,
+                        name: ads.name,
+                        location: ads.location,
+                        smallDes: ads.smallDes,
+                        longDes: ads.longDes,
+                        help: ads.help,
+                        
+                    };
+                }),
+                didDrawPage: function (data) {
+                    const pageSize = doc.internal.pageSize;
+                    const pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
+                    const pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
+                    const x = pageWidth / 2;
+                    const y = pageHeight / 2;
+                    doc.setFontSize(35);
+                    doc.setTextColor(255, 128, 128);
+                    doc.text(watermarkTitle, x, y, null, null, 'center');
+
+                }
+            })
+        doc.save('Advertistment Summary Report.pdf')
+
+    }
 
     const onSearch = (value) => console.log(value);
 
@@ -153,7 +207,7 @@ const Ads = () => {
                                         width: 200,
                                     }}
                                 />
-                                <Button icon={<FilePdfOutlined style={{ fontSize: '22px', color: 'red' }} />} />
+                                <Button icon={<FilePdfOutlined style={{ fontSize: '22px', color: 'red' }} />} onClick={generatePdf}/>
                             </CustomRow>
                         </WrapperCard>
                         <Table columns={Columns} dataSource={ads}
