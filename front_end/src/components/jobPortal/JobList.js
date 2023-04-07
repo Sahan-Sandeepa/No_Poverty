79993433ce779e3cfe29f784a2bc601746c97ba6
IsuruSanjaya <isurusanjayaa@gmail.com>
2023-04-07 22:49:14 +0530

    added PDF

--- front_end/src/components/jobPortal/JobList.js
+++ front_end/src/components/jobPortal/JobList.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { Table, Icon, Button, Space, Input, Col } from 'antd';
+import { Table, Icon, Button, Row, Input, Col } from 'antd';
 import axios from "axios";
 import { EditTwoTone, DeleteOutlined, DeleteTwoTone, DownloadOutlined, FilePdfOutlined, FilePdfTwoTone, SelectOutlined, MessageOutlined } from '@ant-design/icons';
 import CustomRow from '../common/Form_header';
 import WrapperCard from '../common/Wrapper_card';
 import JobPost from './JobPost';
 import { Link, useParams, useNavigate } from 'react-router-dom'
-
+import jsPDF from 'jspdf'
+import 'jspdf-autotable'
 
 const { Search } = Input;
 
@@ -78,6 +79,54 @@ const JobList = () => {
     };
 
     //tables header
+    //added pdf method
+    const generatePdf = () => {
+        const watermarkTitle = 'Job List Report';
+        // Create the PDF document
+        var doc = new jsPDF();
+        doc.setFontSize(10);
+        doc.setFont('helvetica', 'bold');
+        doc.text(10, 10, 'Job List Summary');
+        doc.setFillColor(220, 220, 220);
+        doc.rect(0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight(), 'F');
+        //header and columns of the pdf
+        doc.autoTable(
+            {
+                columns: [
+                    { header: 'Job Title', dataKey: 'jobTitle' },
+                    { header: ' Company', dataKey: 'company' },
+                    { header: 'Location', dataKey: 'location' },
+                    { header: 'Opening Date', dataKey: 'openingDate' },
+                    { header: 'Closing Date', dataKey: 'closingDate' },
+
+
+                ],
+                body: JobList.map(JobList => {
+                    return {
+                        Row: Row,
+                        jobTitle: JobList.jobTitle,
+                        company: JobList.company,
+                        location: JobList.location,
+                        openingDate: JobList.openingDate,
+                        closingDate: JobList.closingDate,
+                    };
+                }),
+                didDrawPage: function (data) {
+                    const pageSize = doc.internal.pageSize;
+                    const pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
+                    const pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
+                    const x = pageWidth / 2;
+                    const y = pageHeight / 2;
+                    doc.setFontSize(65);
+                    doc.setTextColor(255, 128, 128);
+                    doc.text(watermarkTitle, x, y, null, null, 'center');
+
+                }
+            })
+        doc.save('Job List Report.pdf')
+
+    }
+
 
     const Columns = [{
         title: 'Job Title',
@@ -150,7 +199,7 @@ const JobList = () => {
                                 }}
 
                             />
-                            <Button icon={<FilePdfOutlined style={{ fontSize: '21px', color: 'red' }} />} />
+                            <Button icon={<FilePdfOutlined style={{ fontSize: '21px', color: 'red' }} onClick={generatePdf} />} />
                         </CustomRow>
                     </WrapperCard>
                     <Table columns={Columns} dataSource={jobList.filter((jobList) =>
