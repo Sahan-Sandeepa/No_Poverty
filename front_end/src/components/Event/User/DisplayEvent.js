6ce5e87fbbccf568ed6d5cf44ea0bce3169d78ae
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-05-09 21:38:21 +0530

    created user side event file

--- front_end/src/components/Event/User/DisplayEvent.js
+++ front_end/src/components/Event/User/DisplayEvent.js
@@ -1,22 +1,20 @@
-import { Card, Modal } from 'antd';
-import React, { useEffect, useState } from "react";
-import { Form, Input, Button, notification } from "antd";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Card, Modal, Button, Input, Form, notification } from "antd";
 import WrapperCard from '../../common/Wrapper_card';
 import CustomRow from '../../common/Form_header';
 import '../Event-Main.css';
 import { ReadOutlined } from '@ant-design/icons';
-import axios from 'axios';
+import FormItem from "antd/es/form/FormItem";
 
 const DisplayEvent = () => {
-
     const [modalVisible, setModalVisible] = useState(false);
     const [participateModalVisible, setParticipateModalVisible] = useState(false);
     const [eventDetails, setAllEventDetails] = useState([]);
-
     const [id, setid] = useState("");
     const [name, setname] = useState("");
-    const [eventId, setEventId] = useState();
-    console.log(eventId)
+    const [eventId, setEventId] = useState("");
+    const [selectedEventId, setSelectedEventId] = useState(null);
 
     function getAllEventDetails() {
         axios
@@ -24,32 +22,38 @@ const DisplayEvent = () => {
             .then((res) => {
                 console.log(res);
                 setAllEventDetails(res.data.Event);
-                setEventId(res.data.Event[0]._id);
+                setEventId(res.data.Event[0]._id); // set the first event ID as default
             })
             .catch(() => {
                 alert("Check The Connectivity");
             });
     }
-    // console.log(eventDetails);
-    useEffect(() => getAllEventDetails(), []);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // get the form data from state or refs
-        const EventSchema = {
+    useEffect(() => {
+        getAllEventDetails();
+    }, []);
+
+    const handleSubmit = () => {
+        // e.preventDefault();
+
+        const eventSchema = {
             id,
-            name
+            name,
         };
         axios
-            .post(`http://localhost:4000/event/${eventId}/registered-entities`, EventSchema)
+            .post(
+                `http://localhost:4000/event/${eventId}/registered-entities`,
+                eventSchema
+            )
             .then(() => {
                 window.location.reload(false);
             })
             .catch((err) => {
                 alert(err);
-                console.log(EventSchema)
             });
+        console.log(eventSchema);
     };
+
     const showModal = () => {
         setModalVisible(true);
     };
@@ -58,82 +62,126 @@ const DisplayEvent = () => {
         setModalVisible(false);
     };
 
+    // const showParticipateModal = () => {
+    //     setModalVisible(false);
+    //     setParticipateModalVisible(true);
+    // };
+
     const showParticipateModal = () => {
         setModalVisible(false);
         setParticipateModalVisible(true);
     };
 
-    const handleParticipateCancel = () => {
-        setParticipateModalVisible(false);
-    };
-
-    const onFinish = (values) => {
-        console.log("Received values of form: ", values);
-        notification.success({
-            message: "Success",
-            description: "Participation successful"
-        });
-        setParticipateModalVisible(false);
+    const handleEventSelect = (eventId) => {
+        setEventId(eventId);
+        setSelectedEventId(eventId);
     };
 
     return (
         <div className="main-container">
-
             <div className="sub-container">
-
                 <WrapperCard style={{ backgroundColor: "#37475E" }}>
-                    <CustomRow style={{ justifyContent: "space-between", padding: "16px" }}>
-                        <h1 style={{ color: "White" }}>Upcoming Events</h1>
+                    <CustomRow
+                        style={{ justifyContent: "space-between", padding: "16px" }}
+                    >
+                        <h1 style={{ color: "white" }}>Upcoming Events</h1>
                     </CustomRow>
                 </WrapperCard>
-
-                <div style={{ display: 'flex', justifyContent: 'center', margin: '5%' }}>
-
+                <div
+                    style={{ justifyContent: "center", margin: "5%" }}
+                >
                     {eventDetails.map((eventDetailsVal) => (
-                        <Card title={eventDetailsVal.eventName} bordered={false} style={{ marginBottom: "20px", width: "100%" }} extra={eventDetailsVal.eventStatus}>
-                            <Card style={{ width: '100%' }} title="Description">
-                                {eventDetailsVal.eventPlace}
-                            </Card>
-                            <Card style={{ width: '100%' }} title="Date">
-                                {eventDetailsVal.eventDate}
-                            </Card>
-                            <p>{eventDetailsVal.eventPlace.length > 124 ? eventDetailsVal.eventPlace.substring(0, 124) + " ..." : eventDetailsVal.eventPlace}</p>
-                            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                                <ReadOutlined onClick={showModal} style={{ color: "#5DBB63", cursor: "pointer", marginLeft: "20px" }} />
+                        <Card
+                            key={eventDetailsVal._id}
+                            title={eventDetailsVal.eventName}
+                            bordered={false}
+                            style={{ marginBottom: "20px", width: "100%" }}
+                            extra={eventDetailsVal.eventStatus}
+                            onClick={() => handleEventSelect(eventDetailsVal._id)}
+                        >
+                            <div style={{ display: "flex" }}>
+                                <Card style={{ width: "100%" }} title="Location">
+                                    {eventDetailsVal.eventPlace}
+                                </Card>
+                                <Card style={{ width: "100%" }} title="Date">
+                                    {eventDetailsVal.eventDate}
+                                </Card>
                             </div>
-                            <Modal visible={modalVisible} onCancel={handleCancel} footer={null} destroyOnClose={true}>
-                                <p>{eventDetailsVal.eventPlace}</p>
+                            <Card style={{ width: "100%" }} title="Description">
+                                <p>{eventDetailsVal.eventDetails.length > 124 ? eventDetailsVal.eventDetails.substring(0, 120) + " ..." : eventDetailsVal.eventDetails}</p>
+                                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                                    <ReadOutlined onClick={() => {
+                                        if (eventDetailsVal._id === selectedEventId) {
+                                            setModalVisible(true);
+                                        }
+                                    }} style={{ color: "#5DBB63", cursor: "pointer", marginLeft: "20px" }} />
+
+                                </div>
+                            </Card>
+                            {/* <CustomRow style={{ justifyContent: "flex-end" }}>
+                                <ReadOutlined onClick={showModal} style={{ color: "#5DBB63", cursor: "pointer", marginLeft: "20px" }} />
+                            </CustomRow> */}
+
+                            {eventDetailsVal._id === selectedEventId && (
+
+                                <Modal
+                                    open={modalVisible}
+                                    onCancel={handleCancel}
+                                    footer={null}
+                                >
+                                    <Form onFinish={handleSubmit}>
+
+                                        <FormItem>
+                                            <Card style={{ width: "100%" }} title="Description">
+                                                <p>{eventDetailsVal.eventDetails}</p>
+                                            </Card>
+                                            <h3 style={{ display: "flex", justifyContent: "center" }}>Participate in Event</h3>
+                                        </FormItem>
+                                        <Form.Item
+                                            label="Name"
+                                            name="name"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "Please input your name!",
+                                                },
+                                            ]}
+                                        >
+                                            <Input onChange={(e) => setname(e.target.value)} />
+                                        </Form.Item>
+                                        <Form.Item
+                                            label="ID No"
+                                            name="id"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "Please input your ID!",
+                                                },
+                                            ]}
+                                        >
+                                            <Input onChange={(e) => setid(e.target.value)} />
+                                        </Form.Item>
+
+                                        <CustomRow style={{ justifyContent: "flex-end" }}>
+                                            <Button type="primary" htmlType="submit">
+                                                Participate in Event
+                                            </Button>
+                                        </CustomRow>
+                                    </Form>
+                                </Modal>
+                            )}
+
+                            {/* <Modal open={modalVisible} onCancel={handleCancel} footer={null} destroyOnClose={true}>
+                                <p>{eventDetailsVal.eventDetails}</p>
                                 <div style={{ display: "flex", justifyContent: "flex-end" }}>
                                     <Button onClick={showParticipateModal} style={{ backgroundColor: "#5DBB63", color: "#fff" }}>Participate</Button>
                                 </div>
-                            </Modal>
-                            <Modal visible={participateModalVisible} onCancel={handleParticipateCancel} footer={null} destroyOnClose={true}>
-                                <Form
-                                    onFinish={onFinish}
-                                    onSubmitCapture={handleSubmit}
-                                >
-                                    <Form.Item name="name" label="Name" rules={[{ required: true }]}>
-                                        <Input placeholder="Enter your name"
-                                            onChange={(e) => {
-                                                setid(e.target.value);
-                                            }} />
-                                    </Form.Item>
-                                    <Form.Item name="id" label="ID Number" rules={[{ required: true }]}>
-                                        <Input placeholder="Enter your ID number"
-                                            onChange={(e) => {
-                                                setname(e.target.value);
-                                            }} />
-                                    </Form.Item>
-                                    <Form.Item style={{ textAlign: "center" }}>
-                                        <Button type="primary" htmlType="submit" style={{ backgroundColor: "#5DBB63", color: "#fff" }}>Submit</Button>
-                                    </Form.Item>
-                                </Form>
-                            </Modal>
+                            </Modal> */}
                         </Card>
                     ))}
                 </div>
-            </div>
 
+            </div>
         </div>
     );
 };
