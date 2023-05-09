0c135a36cdb40392d98c3f7564dd313fc7d6115d
GRieZzLEO <chamodhleo1@gmail.com>
2023-05-09 23:43:03 +0530

    Added Card UI

--- front_end/src/components/DoDonations/MakeDonations.js
+++ front_end/src/components/DoDonations/MakeDonations.js
@@ -161,6 +161,7 @@ const DonateForm = () => {
                         offset: 8,
                     }}
                 >
+                    
                     <Button type="primary" htmlType="submit" onClick={sendDonation}>
                         Submit
                     </Button>
